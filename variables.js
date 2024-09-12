02ee6210056eb82af2b3ea75f88c635fb3f7841a
patricia145 <patriciavaldez0517@gmail.com>
2024-09-12 12:40:07 +0800

    initial commit

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Create a variable named name and store your name
+let name = 'Valdez, Patricia G.';
+
+// Create a variable named whatDoYouWannaBecomeInYourLife and store "programmer"
+let whatDoYouWannaBecomeInYourLife = 'programmer';
+
+// Create a variable named gender and store your gender
+let gender = 'Female';
+
+// Create a variable named TwitterHandle and store your Twitter Handle
+let TwitterHandle = '@p4trixia';
+
+// Log all the variables to the console
+console.log('What is your Name? ', name);
+console.log('What do you wanna become in your life? ',whatDoYouWannaBecomeInYourLife);
+console.log('Are you Male or Female? ', gender);
+console.log('My Twitter: ', TwitterHandle);
